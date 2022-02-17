3c98fbb4e51d7f6b344765383192d8778fe2837d
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2022-02-17 16:37:48 +0500

    advanced-beginner-level:routing advanced

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -14,3 +14,25 @@ Route::get('/', function (Request $request) {
 });
 
 Route::get('/user', [MyController::class, 'index']);
+
+
+Route::middleware('throttle:4,1')->group(function () {
+    Route::get('/rate-limiting', function () {
+        return response()->json([
+            'data' => [
+                'rate-limit' => '4'
+            ]
+        ], 200);
+    });
+});
+
+
+Route::domain('{account}.myapp.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        return response()->json([
+            'data' => [
+                'route-domain' => 'set domain api'
+            ]
+        ], 200);
+    });
+});
